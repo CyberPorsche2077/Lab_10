9e173fdc7301a393ec396916a86035975bc02ba8
Porsche <peerawit1142@gmail.com>
2023-12-19 16:17:04 +0700

    lab10_3Complete

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    int count=0;
+    double mean=0,deviation=0,sum=0,sumpow2;
+    string textline;
+    ifstream source;
+    source.open("score.txt");
+    while (getline(source,textline))
+    {
+        sum+= atof(textline.c_str());
+        sumpow2+= pow(atof(textline.c_str()),2.0);
+        count++;
+    }
+    mean=sum/count;
+    deviation=sqrt((sumpow2/count)-pow(sum/count,2.0));
+    source.close();
+
+    cout << "Number of data = "<< count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = "<< mean <<"\n";
+    cout << setprecision(3);
+    cout << "Standard deviation = "<< deviation <<"\n";
+}
